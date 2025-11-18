0427f309f6316d39b32f29aabefae017e95fc7ea
tg853309 <tgou58@gmail.com>
2025-11-18 08:24:14 +0100

    Créez vos propres classes

--- index.php
+++ index.php
@@ -5,20 +5,6 @@ const RESULT_LOSER = -1;
 const RESULT_DRAW = 0;
 const RESULT_POSSIBILITIES = [RESULT_WINNER, RESULT_LOSER, RESULT_DRAW];
 
-function probabilityAgainst(int $levelPlayerOne, int $againstLevelPlayerTwo)
-{
-    return 1/(1+(10 ** (($againstLevelPlayerTwo - $levelPlayerOne)/400)));
-}
-
-function setNewLevel(int &$levelPlayerOne, int $againstLevelPlayerTwo, int $playerOneResult)
-{
-    if (!in_array($playerOneResult, RESULT_POSSIBILITIES)) {
-        trigger_error(sprintf('Invalid result. Expected %s',implode(' or ', RESULT_POSSIBILITIES)));
-    }
-
-    $levelPlayerOne += (int) (32 * ($playerOneResult - probabilityAgainst($levelPlayerOne, $againstLevelPlayerTwo)));
-}
-
 $greg = 400;
 $jade = 800;
 
@@ -38,3 +24,25 @@ echo sprintf(
 );
 
 exit(0);
+
+class Player
+{
+    public int $level;
+}
+
+class Encounter
+{
+    public function probabilityAgainst(int $levelPlayerOne, int $againstLevelPlayerTwo)
+    {
+        return 1/(1+(10 ** (($againstLevelPlayerTwo - $levelPlayerOne)/400)));
+    }
+
+    function setNewLevel(int &$levelPlayerOne, int $againstLevelPlayerTwo, int $playerOneResult)
+{
+    if (!in_array($playerOneResult, RESULT_POSSIBILITIES)) {
+        trigger_error(sprintf('Invalid result. Expected %s',implode(' or ', RESULT_POSSIBILITIES)));
+    }
+
+    $levelPlayerOne += (int) (32 * ($playerOneResult - probabilityAgainst($levelPlayerOne, $againstLevelPlayerTwo)));
+}
+}
